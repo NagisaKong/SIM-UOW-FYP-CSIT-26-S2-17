f523dbcbe4dbaebb3253549e3b58aca617b39590
nagisa <zh458574708@outlook.com>
2026-06-19 20:17:43 +0800

    modified:   frontend/admin.html modified:   frontend/app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,6 @@
 // Shared front-end utilities.
 // Point this at your running FastAPI host:
-const API_BASE = "http://127.0.0.1:8001";
+const API_BASE = "http://127.0.0.1:8000";
 
 function authHeader() {
   const token = localStorage.getItem("token");
